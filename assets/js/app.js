e499b04d61f31962a5ad2e9c44826901ba8ec1cd
岸波　雄樹 <you_work_24@icloud.com>
2019-12-19 13:17:44 +0800

    [UPDATE] updated components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../css/app.css';
+import Home from './components/Home';
+    
+ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
 /*
  * Welcome to your app's main JavaScript file!
  *
